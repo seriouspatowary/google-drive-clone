51d8a79ad394d793f182fca715f6c9d05149fdf5
Simanta Patowary <simantapatowary963@gmail.com>
2025-05-27 10:14:22 +0530

    27th may

--- src/lib/better-auth/auth.ts
+++ src/lib/better-auth/auth.ts
@@ -3,12 +3,17 @@ import { nextCookies } from "better-auth/next-js";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import client from "./db";
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "../env";
+import { createAuthMiddleware } from "better-auth/api";
+import { sendMessage } from "@/lib/notification"
+import db from "../database/db";
+import { Subscription } from "../database/schema/subscription.model";
+import { ObjectId } from "mongodb";
 
-const db = client.db();
+const dbClient = client.db();
 
  
 export const auth = betterAuth({
-      database: mongodbAdapter(db),
+      database: mongodbAdapter(dbClient),
 
      socialProviders: {
         google: { 
@@ -16,5 +21,46 @@ export const auth = betterAuth({
             clientSecret: GOOGLE_CLIENT_SECRET as string, 
         }, 
     },
+     
+     hooks: {
+        after: createAuthMiddleware(async (ctx) => {
+            const newSession = ctx.context.newSession;
+            const user = newSession?.user;
+          
+            if (newSession && user) {
+                try {
+                    await db()
+                    const isSubAvail = await Subscription.findOne({ subscriber: user.id });
+
+                    if (isSubAvail) {
+                        return;
+                    }
+                    const subs = await Subscription.create({
+                        subscriber: user.id,
+                        status:"activated"
+                        
+                    })
+
+                    const userCollection = dbClient.collection("user");
+
+                    await userCollection.updateOne({
+                        _id: new ObjectId(subs.subscriber)
+                    }, {
+                      $set:{
+                        subscription:subs._id
+                      }  
+                    }
+                    )
+                        
+                } catch (error) {
+                    console.log("error in creating subsciption beforef hook",error)
+                    throw ctx.redirect("/")
+ 
+                }
+                
+            }
+        }),
+    },
+
     plugins: [nextCookies()] 
 })
